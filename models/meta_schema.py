66dade1fe784cc43480aa4c74e9ca40645dc7c2f
Ryan Collingwood <ryancollingwood@gmail.com>
2023-09-08 16:05:25 +1000

    demo generating a contract

--- models/meta_schema.py
+++ models/meta_schema.py
@@ -1,7 +1,8 @@
-from typing import List, Optional
-from pydantic import Field
+from typing import List, Optional, Dict, Any
+from pydantic import Field, validator
 
-from models import SchemaType
+from .schema_type import SchemaType
+from .cardinality import Cardinality
 from .meta_schema_base_model import MetaSchemaModel, MetaSchemaContainerModel
 
 
@@ -24,16 +25,63 @@ class DatabaseColumn(MetaSchemaModel):
   is_unique: bool
   references: Optional['DatabaseColumn'] = Field(default = None)
 
+  def to_contract(self):
+    return {
+      'database': self.table.database.name,
+      'table': self.table.name,
+      'column': self.name,
+      'schema_type': str(self.schema_type.value),
+      'not_null': self.not_null,
+      'is_unique': self.is_unique,      
+      'references': self.references.to_contract() if self.references else None
+    }
+
 class Property(MetaSchemaContainerModel):
   attribute: PropertyAttribute
   source: Optional[DatabaseColumn] = Field(default = None)
   is_identifier: bool = Field(default=False)
+  
+  def name(self):
+      return self.attribute.name
+
+  def to_contract(self):
+    allows_zero = self.cardinality in [Cardinality.ZERO_OR_ONE, Cardinality.ZERO_OR_MANY]
+
+    result = {
+      'semantic_type': self.attribute.semantic_type.get_meta_schema_label(),
+      'is_identifier': self.is_identifier,
+      'optional': allows_zero,
+    }
+
+    if self.source:
+      result['source'] = self.source.to_contract()
+
+    if self.cardinality not in [Cardinality.ONLY_ONE, Cardinality.ZERO_OR_ONE]:
+      result['type'] = 'list'
+    
+    return result
 
 class Aggregate(MetaSchemaModel):
   properties: List[Property]
+  
+  def to_contract(self):
+    return {
+      self.name: [{p.get_meta_schema_label():p.to_contract() for p in self.properties}]
+    }
 
 class EventAggregate(MetaSchemaContainerModel):
   aggregate: Aggregate
+
+  def name(self):
+    return self.aggregate.name
+
+  def to_contract(self):
+    allows_zero = self.cardinality in [Cardinality.ZERO_OR_ONE, Cardinality.ZERO_OR_MANY]
+    result = self.aggregate.to_contract()
+    result['optional'] = allows_zero
+    if self.cardinality not in [Cardinality.ONLY_ONE, Cardinality.ZERO_OR_ONE]:
+      result['type'] = 'list'
+    return result
 
 class Actor(MetaSchemaModel):
   pass
@@ -42,3 +90,12 @@ class Event(MetaSchemaModel):
   raised_by: Actor
   received_by: Actor
   aggregates: List[EventAggregate]
+
+  def to_contract(self):
+    result = {
+      'event_name': self.name,
+      'raised_by': self.raised_by.name,
+      'received_by': self.received_by.name,
+      'data': [a.to_contract() for a in self.aggregates]
+    }
+    return result
--- models/meta_schema_base_model.py
+++ models/meta_schema_base_model.py
@@ -6,8 +6,35 @@ class MetaSchemaBaseModel(BaseModel):
         anystr_strip_whitespace=True, anystr_lower=True, validate_all=True, extra=Extra.ignore,
       )
 
+    def get_meta_schema_label(value: "MetaSchemaBaseModel"):
+        label = None
+
+        if isinstance(value, MetaSchemaModel):
+            label = value.name
+        elif isinstance(value, MetaSchemaContainerModel):
+            if hasattr(value, "name"):
+                label = value.name()
+            else:
+                # todo raise a warning
+                # print("Warning: no name attribute found for MetaSchemaContainerModel:", value)
+                for key, val in value.__dict__.items():
+                    if isinstance(val, MetaSchemaBaseModel):
+                        label = f"{key}_{val.name}"
+                        break
+        elif isinstance(value, dict):
+            label = value.get("name")
+            
+        if label is None:
+            raise ValueError(f"Could not determine label for {type(value)}: {value}")
+
+        return label
+    
+    def to_contract(self):
+        return self.get_meta_schema_label()
+    
 class MetaSchemaContainerModel(MetaSchemaBaseModel):
     cardinality: Cardinality
+            
 
 class MetaSchemaModel(MetaSchemaBaseModel):
     name: str
