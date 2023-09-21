eaa3890939cf66976fc05e8ed454fbeb67053055
Ryan Collingwood <ryancollingwood@gmail.com>
2023-09-21 19:41:37 +1000

    reformat column_ranges.py

--- capture_sheet/column_ranges.py
+++ capture_sheet/column_ranges.py
@@ -7,12 +7,36 @@ from .column_names import SEMANTIC_TYPE, DATA_CLASSIFICATION
 from .column_names import SCHEMA_TYPE, DATABASE, TABLE, COLUMN, NOT_NULL, IS_UNIQUE
 from .column_names import REFERENCE_DATABASE, REFERENCE_TABLE, REFERENCE_COLUMN
 
-EVENT_COLUMNS = (EVENT, RAISED_BY, RECEIVED_BY,)
-ENTITY_COLUMNS = (ENTITY, ENTITY_CARDINALITY,)
-PROPERTY_COLUMNS = (ATTRIBUTE,ATTRIBUTE_CARDINALITY,)
-ATTRIBUTE_COLUMNS = (SEMANTIC_TYPE, DATA_CLASSIFICATION,)
-SOURCE_COLUMNS = (SCHEMA_TYPE, DATABASE, TABLE, COLUMN, NOT_NULL, IS_UNIQUE,)
-REFERENCE_COLUMNS = (REFERENCE_DATABASE, REFERENCE_TABLE, REFERENCE_COLUMN,)
+EVENT_COLUMNS = (
+    EVENT,
+    RAISED_BY,
+    RECEIVED_BY,
+)
+ENTITY_COLUMNS = (
+    ENTITY,
+    ENTITY_CARDINALITY,
+)
+PROPERTY_COLUMNS = (
+    ATTRIBUTE,
+    ATTRIBUTE_CARDINALITY,
+)
+ATTRIBUTE_COLUMNS = (
+    SEMANTIC_TYPE,
+    DATA_CLASSIFICATION,
+)
+SOURCE_COLUMNS = (
+    SCHEMA_TYPE,
+    DATABASE,
+    TABLE,
+    COLUMN,
+    NOT_NULL,
+    IS_UNIQUE,
+)
+REFERENCE_COLUMNS = (
+    REFERENCE_DATABASE,
+    REFERENCE_TABLE,
+    REFERENCE_COLUMN,
+)
 
 EVENT_PREFIX = "event_"
 ENTITY_PREFIX = "entity_"
@@ -31,14 +55,23 @@ COLUMN_MAP = {
 }
 
 
-def column_subset(df: pd.DataFrame, start_col_name: str, end_col_name: str, end_is_inclusive: bool = True):
+def column_subset(
+    df: pd.DataFrame,
+    start_col_name: str,
+    end_col_name: str,
+    end_is_inclusive: bool = True,
+):
     columns: List[str] = list(df.columns)
     start_index = columns.index(start_col_name)
-    end_index = columns.index(end_col_name) + 1 if end_is_inclusive else columns.index(end_col_name)
+    end_index = (
+        columns.index(end_col_name) + 1
+        if end_is_inclusive
+        else columns.index(end_col_name)
+    )
 
     if start_index == end_index:
         return ValueError("Cannot get a column subset, columns have same index")
     if end_index < start_index:
         return ValueError("End column name is before start column name")
-    
+
     return columns[start_index:end_index]
