3c3e779a2928191818c90c64b3cbc2bffad90f3f
Ryan Collingwood <ryancollingwood@gmail.com>
2023-10-05 20:27:17 +1100

    `identifiers` property on `EventAggregate`
    - remade `is_identifier` on  `Property`

--- models/meta_schema.py
+++ models/meta_schema.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from pydantic import Field
+from pydantic import Field, validator
 
 from .schema_type import SchemaType
 from .data_classification import DataClassification
@@ -75,10 +75,13 @@ class DatabaseColumn(MetaSchemaModel):
 class Property(MetaSchemaContainerModel):
     attribute: PropertyAttribute
     source: Optional[DatabaseColumn] = Field(default=None)
-    is_identifier: bool = Field(default=False)
 
     def name(self):
         return self.attribute.name
+    
+    @property
+    def is_identifier(self):
+        return self.attribute.semantic_type.variety.is_unique()
 
 
 class Aggregate(MetaSchemaModel):
@@ -87,9 +90,22 @@ class Aggregate(MetaSchemaModel):
 
 class EventAggregate(MetaSchemaContainerModel):
     aggregate: Aggregate
+    # TODO: this is a hack to get the identifiers 
+    # `repr=False` means it won't be considered in output for `__repr__`
+    identifiers: Optional[List[str]] = Field(None, repr=False)
 
     def name(self):
         return self.aggregate.name
+    
+    @validator("identifiers", always=True)
+    def get_indentifiers(cls, v, values, **kwargs):
+        """
+        Tried this with @computed_field but it didn't work
+        So using a validator instead as per:
+        https://github.com/pydantic/pydantic/issues/1928#issuecomment-692366291
+        """
+        result = [x.slug for x in values["aggregate"].properties if x.is_identifier]
+        return result
 
 
 class Actor(MetaSchemaModel):
