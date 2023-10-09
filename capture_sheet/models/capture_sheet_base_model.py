d691300e300de189418207265edbf00973cc17a5
Ryan Collingwood <ryancollingwood@gmail.com>
2023-10-09 19:44:42 +1100

    Pydantic String Config, Removal of Str to Lower processing

--- capture_sheet/models/capture_sheet_base_model.py
+++ capture_sheet/models/capture_sheet_base_model.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, ConfigDict, Extra
 
 class CaptureSheetBaseModel(BaseModel):
     model_config: ConfigDict = ConfigDict(
-        str_to_lower=True, 
         validate_default=True, 
         extra=Extra.allow,
         # this allows us to populate a field by it's alias
