80d8f620c580bd702fbc7275ed0ccaa8ca6f4fe9
Ryan Collingwood <ryancollingwood@gmail.com>
2023-10-25 19:59:59 +1100

    update models export

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from .schema_type import SchemaType
 from .data_classification import DataClassification
 from .meta_schema_base_model import MetaSchemaBaseModel, MetaSchemaModel, MetaSchemaContainerModel
 from .meta_timing import MetaTiming
+from .variety import Variety
 from .meta_schema import (
     Event,
     Actor,
